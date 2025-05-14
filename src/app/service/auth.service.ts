4caa0813d53ff205774872bd4a9f4e3e8a9aa961
Vishal Pandey <vishalpandey10022000@gmail.com>
2025-05-14 19:01:01 +0530

    Update auth.service.ts

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { catchError, map, Observable, tap, throwError } from 'rxjs';
 import { StorageServiceService } from './storage-service.service';
 
-const BASE_URL = ['https://school-management-java-appreciative-raven-xt.cfapps.us10-001.hana.ondemand.com/'];
+const BASE_URL = ['https://school-management-java-persistent-wolverine-wt.cfapps.us10-001.hana.ondemand.com/'];
 @Injectable({
   providedIn: 'root',
 })
